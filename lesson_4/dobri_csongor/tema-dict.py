13a9505cbe6eadf0629ed736c959d5794536955a
dobricsongor <dobricsongor@yahoo.com>
2020-10-01 18:44:26 +0300

    dict homework

---
+++ lesson_4/dobri_csongor/tema-dict.py
@@ -0,0 +1,28 @@
+datalist = [('Belgium',	2017,	'M',	1.8),('Belgium',	2017,	'F',	2.3),('Bulgaria',	2017,	'M',	1.8),('Bulgaria',	2017,	'F',	1.8),('Czechia',	2017,	'M',	2.1), ('Czechia',	2017,	'F',	2.7),('Denmark',	2017,	'M',	3.3),('Denmark',	2017,	'F',	2.7),('Germany',	2017,	'M',	2.6),('Germany', 	2017,	'F',	2.9),('Estonia',	2017,	'M',	3.6),('Estonia',	2017,	'F',	4.3),('Ireland',	2017,	'M',	0.5),('Ireland',	2017,	'F',	0.8),('Greece',	2017,	'M',	1.7),('Greece',	2017,	'F',	2),('Spain',	2017,	'M',	1.2),('Spain',	2017,	'F',	1.3),('France',	2017,	'M',	3.2),('France',	2017,	'F',	4.3),
+('Croatia',	2017,	'M',	3.6),('Croatia',	2017,	'F',	3.9),('Italy',	2017,	'M',	0.8),('Italy',	2017,	'F',	1.3),('Cyprus',	2017,	'M',	1.7),('Cyprus',	2017,	'F',	0.7),('Latvia',	2017,	'M',	5),('Latvia',	2017,	'F',	6.3),('Lithuania',	2017,	'M',	2.4),('Lithuania',	2017,	'F',	3.3),('Luxembourg',	2017,	'M',	4.2),('Luxembourg',	2017,	'F',	4.1),('Hungary',	2017,	'M',	2.8),('Hungary',	2017,	'F',	3.7),('Malta',	2017,	'M',	0.9),('Malta',	2017,	'F',	0.4),('Netherlands',	2017,	'M',	1.2),('Netherlands',	2017,	'F',	1.6),('Austria',	2017,	'M',	1.8),('Austria',	2017,	'F',	2.4),('Poland',	2017,	'M',	3.7),('Poland',	2017,	'F',	3.7),
+('Portugal',	2017,	'M',	3.8),('Portugal',	2017,	'F',	4.8),('Romania',	2017,	'M',	0.4),('Romania',	2017,	'F',	0.5),('Slovenia',	2017,	'M',	3.7),('Slovenia',	2017,	'F',	3.3),('Slovakia',	2017,	'M',	2.7),('Slovakia',	2017,	'F',	2.7),('Finland',	2017,	'M',	1.4),('Finland',	2017,	'F',	1.8),('Sweden',	2017,	'M',	1.3),('Sweden',	2017,	'F',	2.5),('United_Kingdom',	2017,	'M',	1.7),('United_Kingdom',	2017,	'F',	2.6),('Iceland',	2017,	'M',	0.9),('Iceland',	2017,	'F',	2),('Norway',	2017,	'M',	1.9),('Norway',	2017,	'F',	3),('Switzerland',	2017,	'M',	1.4),('Switzerland',	2017,	'F',	1.6),('Montenegro',	2017,	'M',	2.8),('Montenegro',	2017,	'F',	2.7),('North_Macedonia',	2017,	'M',	2),('North_Macedonia',	2017,	'F',	2.2),('Serbia',	2017,	'M',	5.3),('Serbia',	2017,	'F',	4.7),('Turkey',	2017,	'M',	3.3),('Turkey',	2017,	'F',	4.4),('Belgium',	2018,	'M',2),('Belgium',	2018,	'F',	2.5),('Bulgaria',	2018,	'M',	1.5),('Bulgaria',	2018,	'F',	1.5),('Czechia',	2018,	'M',	2.3),('Czechia',	2018,	'F',	2.8),('Denmark',	2018,	'M',	2.5),('Denmark',	2018,	'F',	3),('Germany',	2018,	'M',	2.5),('Germany', 	2018,	'F',	2.5),('Estonia',	2018,	'M',	4.7),('Estonia',	2018,	'F',	4.4),('Ireland',	2018,	'M',	0.7),('Ireland',	2018,	'F',	0.6),('Greece',	2018,	'M',	1),('Greece',	2018,	'F',	1),('Spain',	2018,	'M',	1.2),('Spain',	2018,	'F',	2),
+('France',	2018,	'M',	2.8),('France',	2018,	'F',	4.3),('Croatia',	2018,	'M',	3.3),('Croatia',	2018,	'F',	3.8),('Italy',	2018,	'M',	0.8),('Italy',	2018,	'F',	1.3),
+('Cyprus',	2018,	'M',	0.9),('Cyprus',	2018,	'F',	0.9),('Latvia',	2018,	'M',	3.1),('Latvia',	2018,	'F',	4.3),('Lithuania',	2018,	'M',	3.2),('Lithuania',	2018,	'F',	2.9),('Luxembourg',	2018,	'M',	4.1),('Luxembourg',	2018,	'F',	5.3),('Hungary',	2018,	'M',	2.7),('Hungary',	2018,	'F',	3),('Malta',	2018,	'M',	0.9),('Malta',	2018,	'F',	1),('Netherlands',	2018,	'M',	1.7),('Netherlands',	2018,	'F',	1.2),
+('Austria',	2018,	'M',	2.1),('Austria',	2018,	'F',	2.2),('Poland',	2018,	'M',	3.4),('Poland',	2018, 'F',	2.9),('Portugal',	2018,	'M',	3),('Portugal',	2018,	'F',	5.3),('Romania',	2018,	'M',	0.5),('Romania',	2018,	'F',	0.4),('Slovenia',	2018,	'M',	3.4),('Slovenia',	2018,	'F',	3.5),('Slovakia',	2018,	'M',	3.2),('Slovakia',	2018,	'F',	3.5),('Finland',	2018,	'M',	1.4),('Finland',	2018,	'F',	1.9),('Sweden',	2018,	'M',	1.6),('Sweden',	2018,	'F',	3.1),('United_Kingdom',	2018,	'M',	2),('United_Kingdom',	2018,	'F',	2.8),
+('Iceland',	2018,	'M',	0),('Iceland',	2018,	'F',	0),('Norway',	2018,	'M',	2.7),('Norway',	2018,	'F',	3.4),('Switzerland',	2018,	'M',	1.5),('Switzerland',	2018,	'F',	1.6),('Montenegro',	2018,	'M',	0),('Montenegro',	2018,	'F',	0),('North_Macedonia',	2018,	'M',	1.8),('North_Macedonia',	2018,	'F',	2.3),('Serbia',	2018,	'M',	3.7),('Serbia',	2018,	'F',	3.7),('Turkey',	2018,	'M',	3.5),('Turkey',	2018,	'F',	5.6)]
+
+
+health_index_2017 = {country+' '+sex: [sex,health_index] for country, year, sex, health_index in datalist if year == 2017}
+health_index_2018 = {country+' '+sex: [sex,health_index] for country, year, sex, health_index in datalist if year == 2018}
+
+print(health_index_2017)
+print(health_index_2018)
+
+germany = {(year,sex) : [health_index] for country, year, sex, health_index in datalist if country == 'Germany'}
+print(germany)
+
+health_index = {country + '_' + str(year) + sex : [year,sex,health_index] for country,year,sex,health_index in datalist}
+print(health_index)
+
+print ({country + '_' + str(year) : [year,sex,health_index] for country,year,sex,health_index in datalist if health_index > 5})
+
+print ({country + '_' + str(year) : [year,sex,health_index] for country,year,sex,health_index in datalist if health_index > 5 and sex == 'F'})
+
+for health in health_index:
+    print (health_index[health][2])
+
