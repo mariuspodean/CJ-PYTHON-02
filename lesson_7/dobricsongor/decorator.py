39e4e9ac9a51b0811bdcf0ea6d993bf0d4c65266
dobricsongor <dobricsongor@yahoo.com>
2020-10-09 13:40:05 +0300

    Lesson-7-dobricsongor

--- lesson_7/dobricsongor/decorator.py
+++ lesson_7/dobricsongor/decorator.py
@@ -2,6 +2,7 @@ def uppercase(fnc):
     
     def inner_func(given_text):
         return(fnc(given_text).upper())
+    
     return inner_func
     
 
@@ -11,3 +12,24 @@ def greet(name):
 
 print(greet("World"))
 
+
+#------------------------------------------------------------------------------
+
+def safe_divide(fnc):
+
+    def inner_func(first,second):
+        if first%second==0:
+            print(f'The result of dividing the numbers {first} and {second} is: ',(int(first/second)))
+        else:
+            print('The operation divide cannot be done!') 
+        return (fnc(first,second))
+
+    return inner_func
+
+@safe_divide
+def divide(first_number, second_number):
+    return first_number / second_number
+
+(divide(10,2))
+
+#----------------------------------------------------------------------------------
