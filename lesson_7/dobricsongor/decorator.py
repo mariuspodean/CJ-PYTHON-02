adbb8efba89389accacf5fe71602d261028decbd
dobricsongor <dobricsongor@yahoo.com>
2020-10-09 14:16:54 +0300

    Lesson-7-dobricsongor

--- lesson_7/dobricsongor/decorator.py
+++ lesson_7/dobricsongor/decorator.py
@@ -1,3 +1,5 @@
+# Create a decorator called uppercase that will uppercase the result
+
 def uppercase(fnc):
     
     def inner_func(given_text):
@@ -14,6 +16,8 @@ print(greet("World"))
 
 
 #------------------------------------------------------------------------------
+
+#Create a decorator called safe_divide that will output a message if the division cannot be performed, othervise it will return the result.
 
 def safe_divide(fnc):
 
@@ -33,3 +37,27 @@ def divide(first_number, second_number):
 (divide(10,2))
 
 #----------------------------------------------------------------------------------
+
+#Create a decorator called register that will update a list called print_registry with all the decorated functions names.
+
+print_registry = []
+
+
+#def register(fnc):
+
+
+
+@register
+def greet(name):
+    return "Greetings {}!".format(name)
+
+
+def say_hello(name):
+    return "Hello {}!".format(name)
+
+@register
+def say_goodbye(name):
+    return "Goodbye {}!".format(name)
+
+print(print_registry)
+#>>> ['greet', 'say_goodbye']
