434917c59a08ffc3cb054b8c3280816c5450da4e
Ioan Gabriel Juglea <jugleaioan@gmail.com>
2020-10-11 16:06:05 +0300

    Homework Lesson 7

---
+++ Lesson-7-Ioan-Juglea/Homework.py
@@ -0,0 +1,47 @@
+def uppercase(func):
+    def inner_func(name):
+        return func(name).upper()
+    return inner_func
+
+
+@uppercase
+def greet(name):
+    return "Greetings {}!".format(name)
+
+print(greet("World"))
+
+def safe_divide(func):
+    def inner_func(first_number, second_number):
+        if second_number == 0:
+            return "The division cannot be performed"
+        else:
+            return func(first_number, second_number)
+    return inner_func
+
+@safe_divide
+def divide(first_number, second_number):
+    return first_number / second_number
+
+print(divide(8, 0))
+
+print_registry = []
+
+def register(func):
+    print_registry.append(func.__name__)
+    def inner_func(name):
+        return func(name)
+    return inner_func
+
+@register
+def greet_(name):
+    return "Greetings {}!".format(name)
+
+
+def say_hello(name):
+    return "Hello {}!".format(name)
+
+@register
+def say_goodbye(name):
+    return "Goodbye {}!".format(name)
+
+print(print_registry)
