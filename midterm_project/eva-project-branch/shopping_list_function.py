ecf4aeab6690a0a604e07ccf06c7e0196b4890e8
evachitul <eva.chitul@coleurope.eu>
2020-11-01 18:09:41 +0100

    Fridge Class

--- midterm_project/eva-project-branch/shopping_list_function.py
+++ midterm_project/eva-project-branch/shopping_list_function.py
@@ -1,19 +1,14 @@
 
 def prepare_shopping_list(fridge, recipe):
     products_fridge = {item.lower(): fridge[item] for item in fridge}
-    print(products_fridge)
-
-    products_recipe = dict(recipe.ingredients.items())
-    print(products_recipe)
 
     shopping_list = {}
 
-    for product, quantity in products_recipe.items():
+    for product, quantity in recipe.ingredients.items():
         if product not in products_fridge:
             shopping_list[product] = quantity
         else:
-            if quantity > products_recipe[product]:
-                shopping_list[product] = quantity - products_recipe[product]
-            break
+            if quantity > products_fridge[product]:
+                shopping_list[product] = quantity - products_fridge[product]
 
     return f'This is the needed shopping list for {recipe.name}: {shopping_list}'
