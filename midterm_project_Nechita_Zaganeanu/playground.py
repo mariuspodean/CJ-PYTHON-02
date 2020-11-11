5aedbd23103b27a8fd5b803cf588151d853da596
Alexandra <alexandra.m.nechita@gmail.com>
2020-11-11 14:28:27 +0200

    Complete code for midterm project

--- midterm_project_Nechita_Zaganeanu/playground.py
+++ midterm_project_Nechita_Zaganeanu/playground.py
@@ -1,5 +1,5 @@
 from shopping_list import Recipe, RecipesBox, Fridge, check_the_fridge, prepare_shopping_list, shopping_list_archive
-from collections.abc import MutableMapping, Mapping, MutableSequence
+from collections.abc import MutableSequence, Mapping, MutableMapping
 
 chicken_soup_ingredients = {
     'chicken': 1,
@@ -59,10 +59,6 @@ dictionary_of_products = {
     'carrots':2
 }
 
-nechita_fridge=Fridge('Nechita\'s Fridge', dictionary_of_products)
-if 'eggs' in nechita_fridge:
-    print('yap')
-
 chicken_soup = Recipe('Grandma\'s chicken soup', chicken_soup_ingredients)
 choccolate_cake = Recipe('Delicious Choccolate Cake', choccolate_cake_ingredients)
 scrumbled_eggs = Recipe('Scrumbled Eggs', scrumbled_eggs_ingredients)
@@ -70,11 +66,18 @@ pork_steak = Recipe('Mediteranean Pork Steak', pork_steak_ingredients)
 pancakes = Recipe('Simple Pancakes', pancakes_ingredients)
 mac_and_cheese = Recipe('Famous Mac & Cheese', mac_and_cheese_ingredients)
 
-my_dict = mac_and_cheese.ingredients_dict()
-my_dict['juice']=3
-# print(mac_and_cheese.ingredients_dict())
+print (choccolate_cake)
 
-recipes_box = RecipesBox([chicken_soup, choccolate_cake, scrumbled_eggs, mac_and_cheese])
+ingredients=list(pancakes.keys())
+print('Lista de ingrediente pentru pancakes este: ', ingredients, '\n')
+
+# print(pork_steak.name)
+# pork_steak.name = 'Maciupiciu'
+# pork_steak.dictionary_of_ingredients['pepper'] = 2
+# print(pork_steak.dictionary_of_ingredients)
+
+recipes_box = RecipesBox('Alexandra\'s list', [chicken_soup, choccolate_cake, scrumbled_eggs, mac_and_cheese])
+
 recipes_box.add(pork_steak)
 recipes_box.add(pancakes)
 recipes_box.delete('Famous Mac & Cheese')
@@ -83,12 +86,42 @@ print(recipes_box)
 del recipes_box[1]
 print(recipes_box)
 
-# print(check_the_fridge(nechita_fridge, recipes_box))
+random_recipe = recipes_box.pick()
+print('Random recipe is:\n', random_recipe)
+picked_recipe = recipes_box.pick('Scrumbled Eggs')
+print('Picked recipe is:\n', picked_recipe)
 
-recipe=recipes_box.pick()
-# print (recipe)
+nechita_fridge = Fridge('Nechita\'s Fridge', dictionary_of_products)
 
-prepare_shopping_list(nechita_fridge, recipe)
-# print (shopping_list_archive)
+print(nechita_fridge)
 
-# print(isinstance(nechita_fridge, MutableMapping))
+if 'eggs' in nechita_fridge:
+    print('yap')
+
+nechita_fridge.add('salmon', 1)
+if 'salmon' in nechita_fridge:
+    print ('yap, there is salmon in the fridge')
+
+nechita_fridge.add('eggs', 3)
+print ('Now there are ', nechita_fridge['eggs'], ' eggs in the fridge')
+
+print('There are ', nechita_fridge['tomatoes'], ' tomatoes in the fridge')
+nechita_fridge.extract('tomatoes', 2)
+print('Now there are ', nechita_fridge['tomatoes'], ' tomatoes in the fridge')
+
+print('There is ', nechita_fridge['butter'], 'butter in the fridge')
+nechita_fridge.extract('butter', 2)
+
+print('There are ', nechita_fridge['carrots'], 'carrots in the fridge')
+nechita_fridge.extract('carrots', 2)
+print ('\n')
+
+nechita_fridge.check_recipe(recipes_box.pick())
+print ('\n')
+
+my_list=check_the_fridge(nechita_fridge, recipes_box)
+if my_list:
+    print (my_list)
+
+prepare_shopping_list(nechita_fridge, recipes_box.pick('Simple Pancakes'))
+print ("Shopping list archive is: ", shopping_list_archive)
--- midterm_project_Nechita_Zaganeanu/shopping_list.py
+++ midterm_project_Nechita_Zaganeanu/shopping_list.py
@@ -1,48 +1,85 @@
 import random
 from collections.abc import MutableSequence, Mapping, MutableMapping
-import copy
 
 class PrettyPrinter:
+    
     def __str__(self):
-        name = self.name()
-        dictionary = self.ingredients_dict()
+        name = self.name
         star_line = '*' * (len(name)+4) + '\n'
         string = star_line + '  '+ name + '\n'
         string += star_line + '\n'     
-        for index, (keys, values) in enumerate(dictionary.items(), 1):
+        for index, (keys, values) in enumerate(self.items(), 1):
             string += f'  {index}. {keys}: {values} \n'
         string += '\n'
         string += star_line
         return (string)
 
-
-class Recipe(PrettyPrinter, Mapping):
+#taken from https://stackoverflow.com/questions/19022868/how-to-make-dictionary-read-only-in-python
+class ReadOnlyDict(dict):
     
-    def __init__(self, recipe_name, dictionary_of_ingredients):
-        self._recipe_name = recipe_name
-        self._dictionary_of_ingredients = dictionary_of_ingredients
-        
+    def __readonly__(self, *args, **kwargs):
+        raise RuntimeError("Cannot modify Recipe")
+    
+    __setitem__ = __readonly__
+    __delitem__ = __readonly__
+    pop = __readonly__
+    popitem = __readonly__
+    clear = __readonly__
+    update = __readonly__
+    setdefault = __readonly__
+    del __readonly__
+    
+class Recipe(PrettyPrinter):
+    
+    def __init__(self, name, dictionary_of_ingredients):
+        self._name = name
+        self._dictionary_of_ingredients = ReadOnlyDict(dictionary_of_ingredients)
+    
+    @property
+    def name(self):
+        return self._name
+
     def __getitem__(self, ingredient):
         return self._dictionary_of_ingredients[ingredient]
+    
+    def __contains__(self, ingredient):
+        if ingredient in self._dictionary_of_ingredients:
+            return True
+        else:
+            return False
+    def get(self, key):
+        return self._dictionary_of_ingredients[key]
+    
+    def __eq__(self, other):
+        if isinstance(other, Recipe):
+            return self._dictionary_of_ingredients == other._dictionary_of_ingredients
+        return False
+    
+    def __ne__(self, other):
+        return not self.__eq__(other)
        
     def __iter__(self):
         return iter(self._dictionary_of_ingredients)       
         
     def __len__(self):
         return len(self._dictionary_of_ingredients)
+    
+    def items(self):
+        return self._dictionary_of_ingredients.items()
+    
+    def keys(self):
+        return self._dictionary_of_ingredients.keys()
+    
+    def values(self):
+        return self._dictionary_of_ingredients.values()
        
-    def name(self):
-        return copy.deepcopy(self._recipe_name)
-    
-    def ingredients_dict (self):
-        return copy.deepcopy(self._dictionary_of_ingredients)
-    
     def __str__(self):
         return super().__str__()
                     
-class RecipesBox (MutableSequence):
+class RecipesBox():
    
-    def __init__ (self, recipes_list):
+    def __init__ (self, name, recipes_list):
+        self.name=name
         self.recipes_list = recipes_list
     
     def __iter__(self):
@@ -66,7 +103,7 @@ class RecipesBox (MutableSequence):
     def pick (self, recipe_to_extract=None):
         if recipe_to_extract:
             for recipe in self.recipes_list:
-                if recipe.recipe_name == recipe_to_extract:
+                if recipe.name == recipe_to_extract:
                     return recipe
         else:
             return random.choice(self.recipes_list)
@@ -76,20 +113,20 @@ class RecipesBox (MutableSequence):
     
     def delete (self, recipe_to_delete):
         for recipe in self.recipes_list:
-            if recipe.name() == recipe_to_delete:
+            if recipe.name == recipe_to_delete:
                 self.recipes_list.remove(recipe)
                 break
     def __str__(self):
         names_list = []
         for recipe in self.recipes_list:
-            names_list.append(recipe._recipe_name)
+            names_list.append(recipe._name)
         return f'Lista de retete este {names_list}'
         
 
-class Fridge(PrettyPrinter, MutableMapping):
+class Fridge(PrettyPrinter):
     
-    def __init__(self, fridge_name, dictionary_of_products):
-        self.fridge_name = fridge_name
+    def __init__(self, name, dictionary_of_products):
+        self.name = name
         self.dictionary_of_products = dictionary_of_products
         
     def __getitem__(self, product):
@@ -106,6 +143,15 @@ class Fridge(PrettyPrinter, MutableMapping):
         
     def __len__(self):
         return len(self.dictionary_of_products)
+    
+    def items(self):
+        return self.dictionary_of_products.items()
+    
+    def keys(self):
+        return self.dictionary_of_products.keys()
+    
+    def values(self):
+        return self.dictionary_of_products.values()
         
     def add (self, product_name, product_quantity):
         previous_quantity=0
@@ -126,18 +172,12 @@ class Fridge(PrettyPrinter, MutableMapping):
             print ("Ingredient not in fridge")
                     
     def check_recipe (self, recipe):
-        recipe_ingredients = set(recipe.dictionary_of_ingredients.keys())
-        fridge_ingredients = set(dictionary_of_products.keys())
+        recipe_ingredients = set(recipe.keys())
+        fridge_ingredients = set(self.dictionary_of_products.keys())
         available_ingredients = list(recipe_ingredients & fridge_ingredients)
         ingredients_to_be_bought = list(recipe_ingredients.difference(fridge_ingredients))
-        print (f'Ingredients to be bought are {ingredients_to_be_bought} and available ingredients are {available_ingredients}')
+        print (f'For the {recipe.name}, ingredients to be bought are {ingredients_to_be_bought} and available ingredients are {available_ingredients}')
         return (ingredients_to_be_bought, available_ingredients)
-    
-    def name (self):
-        return self.fridge_name
-    
-    def ingredients_dict (self):
-        return self.dictionary_of_products
     
     def __str__(self):
         return super().__str__()       
@@ -145,13 +185,13 @@ class Fridge(PrettyPrinter, MutableMapping):
 def check_the_fridge (fridge, recipes_box):
     recipes_from_the_fridge = []
     for recipe in recipes_box:
-        recipe_ingredients = set(recipe.ingredients_dict().keys())
-        fridge_ingredients = set(fridge.ingredients_dict().keys())
+        recipe_ingredients = set(recipe.keys())
+        fridge_ingredients = set(fridge.keys())
         common_ingredients = list(recipe_ingredients & fridge_ingredients)
         if len(common_ingredients) >= len(recipe_ingredients)/2:
             recipes_from_the_fridge.append(recipe.name())
     if not recipes_from_the_fridge:
-        print (f'You don\'t have enough ingredients in the {fridge} for any of the recipes in your {recipes_box}')
+        print (f'You don\'t have enough ingredients in the {fridge.name} for any of the recipes in your {recipes_box.name}')
     return recipes_from_the_fridge
 
 shopping_list_archive=[]
@@ -195,6 +235,7 @@ def pretty_print_recipe(function):
             print (string_to_print)
         else:
             print ("The shopping list is empty")
+        return function(*args)
                     
     return inner_function
 
@@ -203,18 +244,17 @@ def archive_shopping_list(function):
     def inner_function (*args):
         shopping_list_archive.append(function(*args))
         print ('List was archived')
-        return function(*args)
-    return inner_function
+        return function
+    return inner_function 
 
-
-@pretty_print_recipe
 @archive_shopping_list
+@pretty_print_recipe
 def prepare_shopping_list(fridge, recipe):
     shopping_list = {}
-    for ingredient, quantity in recipe.ingredients_dict().items():
-        if ingredient in fridge.ingredients_dict():
-            if fridge.ingredients_dict()[ingredient]<quantity:
-                shopping_list[ingredient]=quantity-fridge.ingredients_dict()[ingredient]
+    for ingredient, quantity in recipe.items():
+        if ingredient in fridge:
+            if fridge[ingredient]<quantity:
+                shopping_list[ingredient]=quantity-fridge[ingredient]
         else:
             shopping_list[ingredient]=quantity
     return shopping_list
