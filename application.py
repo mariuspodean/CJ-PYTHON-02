1d1880c1c82e79b86d2e1d58ec05507dbb770126
Bianca Gabrian <bianca.gabrian94@gmail.com>
2020-11-28 18:29:37 +0200

    updates

--- application.py
+++ application.py
@@ -48,16 +48,140 @@ print (count_active_interns)
 
 show_active_consultants = f'''\n The active interns for the month of {last_month_text} are: \n\n {(october_data.loc[(october_data["Contract type"] == "Freelancer contract") & (october_data['Contract status'] == 'Employed')])}'''
 
-count_active_consultants = f''' \n The active interns for the month of {last_month_text} is number of ID's: \n\n {(october_data.loc[(october_data['Contract type'] == 'Freelancer contract') & (october_data['Contract status'] == 'Employed')]).count()}'''
+count_active_consultants = f''' \n The active interns for the month of {last_month_text} is the number of ID's: \n\n {(october_data.loc[(october_data['Contract type'] == 'Freelancer contract') & (october_data['Contract status'] == 'Employed')]).count()}'''
 
 print (show_active_consultants)
 print (count_active_consultants)
 
+#count and show new joiners
+
+#count and show leavers
 
 
 
+#mutable mapping str and repr methods
+
+from collections.abc import MutableMapping, Mapping, Iterable, Collection, Container, Sized
+
+class Departments(object):
+    def __init__ (self, department):
+        self.department = department
+    def __repr__(self):
+        return repr("Itiviti has the following department: " + self.department + " .")
+    def __str__(self):
+        return 'Itiviti has the following department : {self.department} .'.format(self=self)
+
+class DepartmentsCollection:
+    def __init__(self, departments_list = None):
+        self.departments = list(departments_list) if managers_list else []
+    def __iter__(self):
+        return iter (self.departments)
+    def __getitem__(self,index):
+        return self.departments[index]
+
+departments = [
+    Departments('Legal'),
+    Departments('Infrastructure'),
+    Departments('G&A'),
+    Departments('Professional Services'),
+    Departments('Customer Succes'),
+    Departments ('Global Markets'),
+    Departments ('Product Strategy'),
+    Departments ('Marketing')
+]
+
+print (departments)
+
+class Managers():
+    def __init__ (self, managers):
+        self.managers = managers
+    def __repr__(self):
+        return repr("Itiviti has the following manager: " + self.managers + " .")
+    def __str__(self):
+        return 'Itiviti has the following manager : {self.managers} .'.format(self=self)
+
+managers = [
+    Managers ('Anil S.'),
+    Managers ('Linda M'),
+    Managers ('Karoline R.'),
+    Managers ('Rob M.'),
+    Managers ('Edouard R.'),
+    Managers ('Peter T.'),
+    Managers ('Ofir G.'),
+    Managers ('Joshua M.'),
+    Managers ('Antoine M.'),
+]
+print(managers)
 
 
+
+#Inheritance
+
+class ManagersStartYear(Managers):
+    def __init__(self,managers,year):
+        super().__init__(managers)
+        self.year = year
+    def __str__(self):
+        return '{self.managers} has started in {self.year}. '.format(self=self)
+
+manager_start_year = ManagersStartYear('Anil S.', 2019)
+print (manager_start_year)
+
+#Operator overloading
+
+
+class SeniorityYear: 
+    def __init__(self, year): 
+        self.year = year
+    def __gt__(self, other): 
+        if(self.year>other.year): 
+            return True
+        else: 
+            return False
+
+employee_seniority_1 = SeniorityYear(2019) 
+employee_seniority_2 = SeniorityYear(2020) 
+
+if(employee_seniority_1>employee_seniority_2): 
+    print("First seniority year is greater than second seniority year") 
+else: 
+    print("Second seniority year is greater than first seniority year")
+
+#decorator
+
+#sequence
+
+itiviti_presence_countries_dict= {
+    1: "Australia",
+    2: "Brazil",
+    3: "Canada",
+    4: "France",
+    5: "Hong Kong",
+    6: "India",
+    7: "Italy", 
+    8: "Japan",
+    9: "Philippines", 
+    10: "Romania", 
+    11: "Russia", 
+    12: "Singapore", 
+    13: "Sweden", 
+    14: "United Kingdom",
+    15: "United States"
+}
+
+def country_decorator(fnc):
+    def wrapper():
+        print (u'\U0001f310'+' \u2192')
+        fnc()
+    return wrapper
+
+@country_decorator
+def country_list():
+    print ("Itiviti has offices in the following locations: ")
+    for nr, country in itiviti_presence_countries_dict.items():
+        print("{} {}".format(nr, country))
+
+country_list()
 
 
 
