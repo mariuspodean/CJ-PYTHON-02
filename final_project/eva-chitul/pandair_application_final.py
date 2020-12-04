4fe4025276208aa9c57f6939192bd600e5d11766
evachitul <eva.chitul@coleurope.eu>
2020-12-04 11:51:40 +0100

    Draft Pandair App

---
+++ final_project/eva-chitul/pandair_application_final.py
@@ -0,0 +1,243 @@
+import itertools
+import logging
+import time
+
+fleet_database_check = set()
+flights_log_database = {}
+regional_fleet = {}
+
+log = logging.getLogger('Pandair Airline')
+logging.basicConfig(level=logging.DEBUG, filename='pandair_logging')
+
+
+class Aircraft(object):
+
+    def __init__(self, manufacturer, weight, speed, consumption, identifier, number_flights_maintenance):
+        self.manufacturer = manufacturer
+        self.weight = weight
+        self.speed = speed
+        self.consumption = consumption
+        self.identifier = identifier
+        self.number_flights_maintenance = number_flights_maintenance
+
+    def __str__(self):
+        return f'Aircraft type {type(self).__name__}, identifier {self.identifier}, manufacturer {self.manufacturer}'
+
+    def __repr__(self):
+        return self.identifier
+
+    def due_for_maintenance(self):
+
+        log.info(f' Checking if Aircraft {self.identifier} is due for maintenance')
+        if self.number_flights_maintenance >= 30:
+            return True
+        else:
+            return False
+
+
+class QuickMaintenanceMixin:
+
+    def quick_maintenance(self):
+
+        if self.number_flights_maintenance - 10 < 0:
+            self.number_flights_maintenance = 0
+        else:
+            self.number_flights_maintenance -= 10
+
+        log.debug(f'{time.asctime(time.localtime(time.time()))} {self} completed quick maintenance. Flights number is now {self.number_flights_maintenance}')
+        return f'{self} has gone through quick maintenance. Flights number is now: {self.number_flights_maintenance}'
+
+
+class PassengerAircraft(Aircraft):
+
+    def __init__(self, manufacturer, weight, speed, consumption, identifier, number_flights_maintenance, number_passengers):
+        self.number_passengers = number_passengers
+        super().__init__(manufacturer, weight, speed, consumption, identifier, number_flights_maintenance)
+
+
+class CargoAircraft(Aircraft, QuickMaintenanceMixin):
+
+    def __init__(self, manufacturer, weight, speed, consumption, identifier, load_weight, number_flights_maintenance):
+        self.load_weight = load_weight
+        super().__init__(manufacturer, weight, speed, consumption, identifier, number_flights_maintenance)
+
+    def due_for_maintenance(self):
+        if self.number_flights_maintenance >= 50:
+            return True
+        else:
+            return False
+
+
+class PrivateAircraft(PassengerAircraft, QuickMaintenanceMixin):
+
+    def __init__(self, manufacturer, weight, speed, consumption, identifier, number_passengers, number_flights_maintenance):
+        super().__init__(manufacturer, weight, speed, consumption, identifier, number_flights_maintenance, number_passengers)
+
+
+class CommercialAircraft(PassengerAircraft):
+
+    def __init__(self, manufacturer, weight, speed, consumption, identifier, number_flights_maintenance, number_passengers):
+        super().__init__(manufacturer, weight, speed, consumption, identifier, number_flights_maintenance, number_passengers)
+
+
+class Airport(object):
+
+    def __init__(self):
+        self.airport_list = []
+
+    def __len__(self):
+        return len(self.airport_list)
+
+    def __getitem__(self, index):
+        return self.airport_list[index]
+
+    def __str__(self):
+        return f'Airport: {self.airport_list}'
+
+    def __repr__(self):
+        return f'{self.airport_list}'
+
+    def __add__(self, second_airport):
+        region_airport = Airport()
+        all_aircraft = self.airport_list + second_airport.airport_list
+        for aircraft in all_aircraft:
+            region_airport.add_aircraft(aircraft, check_duplicates=False)
+            log.debug(f'{time.asctime(time.localtime(time.time()))} Created Regional Airport from {self} and {second_airport}')
+        return region_airport
+
+    def add_aircraft(self, aircraft, check_duplicates=True):
+
+        if aircraft in fleet_database_check and check_duplicates:
+            print(f'{aircraft} already in Fleet. Cannot duplicate \n')
+            log.info(f' {aircraft} already in Fleet.')
+        else:
+            self.airport_list.append(aircraft)
+            fleet_database_check.add(aircraft)
+            log.debug(f'{time.asctime(time.localtime(time.time()))} {aircraft} added in Airport and Fleet')
+
+    def remove_aircraft(self, aircraft):
+        if aircraft in self.airport_list:
+            position = self.airport_list.index(aircraft)
+            del self.airport_list[position]
+            fleet_database_check.remove(aircraft)
+            log.debug(f'{time.asctime(time.localtime(time.time()))} {aircraft} removed from Airport and Fleet')
+        else:
+            print(f'{aircraft} not found at Airport. Unable to remove')
+            log.info(f' {aircraft} not found at Airport.')
+
+
+class FleetDatabase(object):
+
+    def __init__(self):
+        self.fleet = {}
+
+    def __getitem__(self, key):
+        if key not in self.fleet:
+            print(f'Airport {key} not in Fleet Database. Airport will be added')
+            log.info(f' Airport {key} not in Fleet Database.')
+            self.fleet[key] = Airport()
+            log.debug(f'{time.asctime(time.localtime(time.time()))} Airport {key} not found. Airport was added to Fleet Database')
+        return self.fleet[key]
+
+    def __delitem__(self, key):
+        del self.fleet[key]
+
+    def __setitem__(self, key, value):
+        self.fleet[key] = value
+
+    def __len__(self):
+        return len(self.fleet)
+
+    def __iter__(self):
+        return iter(self.fleet)
+
+    def __str__(self):
+        return f'Fleet and Location Overview: {self.fleet}'
+
+    def __repr__(self):
+        return f'{self.fleet}'
+
+    def add_airport(self, airport_name, airport_list):
+        if airport_name.title() in self.fleet.keys():
+            print('Airport already in Fleet Database. The new aircrafts will replace the old ones')
+            log.info(f' {airport_name.title()} already in Fleet Database.')
+            for aircraft in self.fleet[airport_name.title()]:
+                fleet_database_check.remove(aircraft)
+                log.debug(f'{time.asctime(time.localtime(time.time()))} {airport_name} removed from Fleet Database. Fleet Overview {fleet_database_check}')
+
+        for new_aircraft in airport_list:
+            fleet_database_check.add(new_aircraft)
+            log.debug(f'{time.asctime(time.localtime(time.time()))} {new_aircraft} added to Fleet Database. Fleet Overview {fleet_database_check}')
+
+        self.fleet[airport_name.title()] = airport_list
+        log.debug(f'{time.asctime(time.localtime(time.time()))} {airport_name.title()} fleet replaced. New {airport_name.title()} {airport_list} ')
+
+    def remove_airport(self, airport_name):
+        if airport_name.title() in self.fleet.keys():
+            for aircraft in self.fleet[airport_name.title()]:
+                fleet_database_check.remove(aircraft)
+                log.debug(f'{time.asctime(time.localtime(time.time()))} {aircraft} removed from Fleet Database. Fleet overview {fleet_database_check}')
+            del self.fleet[airport_name.title()]
+            log.debug(f'{time.asctime(time.localtime(time.time()))} {airport_name.title()} removed from Fleet Database. Overview of airports {self.fleet}')
+        else:
+            print(f'{airport_name.title()} Airport not found in Fleet Database. Unable to remove')
+            log.info(f' {airport_name.title()} not found in Fleet. ')
+
+
+def flights_log(flight):
+
+    def track_flight(*args):
+        results = flight(*args)
+        if results:
+            aircraft, city, destination = results[0], results[1], results[2]
+
+            flights_log_database[f'Entry {len(flights_log_database) + 1}'] = f' Aircraft {aircraft.identifier}: {city} to {destination}'
+            log.info(f' New flight added to flight log: {aircraft.identifier}: {city} to {destination}')
+
+            if aircraft.due_for_maintenance:
+                print(f'Alert: Aircraft {aircraft.identifier} is due for maintenance!')
+                log.info(f' Aircraft Alert: {aircraft.identifier} is due for maintenance!')
+        else:
+            return None
+
+        return flight
+    return track_flight
+
+
+@flights_log
+def operate_flight(fleet_data, city, destination, aircraft):
+
+    if aircraft in fleet_data[city.title()]:
+
+        fleet_data[city.title()].remove_aircraft(aircraft)
+        log.debug(f'{time.asctime(time.localtime(time.time()))} {aircraft} removed from {city.title()} airport. {city.title()} airport overview: {fleet_data[city.title()]}')
+
+        fleet_data[destination.title()].add_aircraft(aircraft)
+        log.debug(f'{time.asctime(time.localtime(time.time()))} {aircraft} added to {destination} airport. {destination.title()} airport overview: {fleet_data[destination.title()]}')
+
+        aircraft.number_flights_maintenance += 1
+        log.debug(f'{time.asctime(time.localtime(time.time()))} {aircraft} number of flights increased by 1. Number of flights operated now at {aircraft.number_flights_maintenance}')
+
+    else:
+        print(f'{aircraft} not in {city}. Cannot perform flight ')
+        log.info(f' {aircraft} not found in {city} airport.')
+        return None
+
+    return aircraft, city, destination
+
+
+def generate_pairs():
+
+    list_origin = set([origin for origin, fleet in fleet_database.items() for aircraft in fleet
+                   if isinstance(aircraft, PassengerAircraft) and aircraft.number_passengers > 100])
+
+    list_destination = set([destination for destination, planes in fleet_database.items() if len(planes) <= 3])
+
+    print(list_origin)
+    print(list_destination)
+
+    for origin, destination in itertools.product(list_origin, list_destination):
+        if origin != destination:
+            log.debug(f'{time.asctime(time.localtime(time.time()))} New origin-destination pair was generated: {origin} - {destination}')
+            yield f'Possible origin destination pair: {origin} - {destination}'
+
---
+++ final_project/eva-chitul/pandair_playground_final.py
@@ -0,0 +1,148 @@
+from pandair_application_final import Aircraft, QuickMaintenanceMixin, PassengerAircraft, PrivateAircraft, CargoAircraft, CommercialAircraft, Airport, FleetDatabase, operate_flight, flights_log, generate_pairs, fleet_database_check, flights_log_database, regional_fleet
+
+#PLAYGROUND
+
+# CREATE AIRCRAFT ALL TYPES
+
+private_1 = PrivateAircraft('Bombardier', 300, 500, 200, 'P1000', 150, 5)
+private_2 = PrivateAircraft('Airbus', 100, 200, 150, 'P2000', 50, 2)
+private_3 = PrivateAircraft('Boeing', 800, 300, 150, 'P3000', 25, 8)
+private_4 = PrivateAircraft('Bombardier', 200, 400, 190, 'P4000', 110, 7)
+
+cargo_1 = CargoAircraft('Airbus', 1000, 700, 400, 'CA100', 1500, 25)
+cargo_2 = CargoAircraft('Boeing', 2000, 1000, 800, 'CA200', 1500, 27)
+cargo_3 = CargoAircraft('Boeing', 2000, 1000, 800, 'CA300', 1500, 30)
+cargo_4 = CargoAircraft('Airbus', 1500, 800, 900, 'CA400', 1000, 20)
+
+commercial_1 = CommercialAircraft('Airbus', 1000, 900, 800, 'CO100', 30, 80)
+commercial_2 = CommercialAircraft('Airbus', 1000, 900, 800, 'CO200', 30, 350)
+commercial_3 = CommercialAircraft('Bombardier', 800, 600, 400, 'CO300', 20, 200)
+commercial_4 = CommercialAircraft('Boeing', 900, 800, 600, 'CO400', 25, 250)
+
+print(private_1)
+print(private_2)
+print(cargo_1)
+print(cargo_2)
+print(commercial_1)
+print(commercial_2)
+
+# CHECK DUE FOR MAINTENANCE
+
+print(commercial_4.due_for_maintenance())
+print(commercial_2.due_for_maintenance())
+print(private_1.due_for_maintenance())
+print(cargo_3.due_for_maintenance())
+print(private_2.due_for_maintenance())
+
+
+# CHECK QUICK MAINTENANCE
+
+print('Private 3 before quick maintenance', private_3.number_flights_maintenance)
+print('Cargo 2 before quick maintenance', cargo_2.number_flights_maintenance, '\n')
+
+print(private_3.quick_maintenance())
+print(cargo_2.quick_maintenance(), '\n')
+
+print('Private 3 after quick maintenance', private_3.number_flights_maintenance)
+print('Cargo 2 after quick maintenance', cargo_2.number_flights_maintenance, '\n')
+
+print(private_1.quick_maintenance())
+print(cargo_1.quick_maintenance(), '\n')
+
+# CREATE AIRPORT, ADD/REMOVE AIRCRAFT FROM AIRPORT
+
+brussels_airport = Airport()
+brussels_airport.add_aircraft(commercial_2)
+brussels_airport.add_aircraft(private_2)
+brussels_airport.add_aircraft(cargo_3)
+brussels_airport.add_aircraft(cargo_3)
+
+cluj_airport = Airport()
+cluj_airport.add_aircraft(commercial_1)
+cluj_airport.add_aircraft(private_2)
+cluj_airport.add_aircraft(private_1)
+cluj_airport.add_aircraft(cargo_1)
+
+berlin_airport = Airport()
+berlin_airport.add_aircraft(private_3)
+berlin_airport.add_aircraft(cargo_2)
+berlin_airport.add_aircraft(cargo_1)
+berlin_airport.add_aircraft(cargo_3)
+
+prague_airport = Airport()
+prague_airport.add_aircraft(private_4)
+prague_airport.add_aircraft(cargo_4)
+prague_airport.add_aircraft(commercial_3)
+prague_airport.add_aircraft(commercial_4)
+
+print('Brussels', brussels_airport, '\n')
+print('Cluj-Napoca', cluj_airport, '\n')
+print('Berlin', berlin_airport, '\n')
+print('Prague', prague_airport, '\n')
+
+brussels_airport.remove_aircraft(cargo_3)
+cluj_airport.remove_aircraft(commercial_1)
+berlin_airport.remove_aircraft(cargo_3)
+prague_airport.remove_aircraft(private_4)
+
+print('Brussels', brussels_airport, '\n')
+print('Cluj-Napoca', cluj_airport, '\n')
+print('Berlin', berlin_airport, '\n')
+print('Prague', prague_airport, '\n')
+
+# ADD TWO AIRPORTS - OPERATION OVERLOAD CHECK
+
+print(brussels_airport)
+print(berlin_airport)
+print('Regional', brussels_airport+berlin_airport)
+print('Regional', prague_airport+berlin_airport)
+
+# CREATE FLEET, ADD/REMOVE AIRPORTS FROM FLEET
+
+fleet_database = FleetDatabase()
+
+print(fleet_database, '\n')
+fleet_database.add_airport('Brussels', brussels_airport)
+fleet_database.add_airport('Cluj-Napoca', cluj_airport)
+fleet_database.add_airport('Berlin', berlin_airport)
+fleet_database.add_airport('Prague', prague_airport)
+print(fleet_database, '\n')
+
+print(fleet_database_check)
+fleet_database.remove_airport('brussels')
+print(fleet_database, '\n')
+print(fleet_database_check, '\n')
+
+# OPERATE FLIGHT
+
+operate_flight(fleet_database, 'Brussels', 'Berlin', commercial_2)
+print(fleet_database, '\n')
+
+operate_flight(fleet_database, 'Berlin', 'Cocomo', private_3)
+print(fleet_database, '\n')
+
+operate_flight(fleet_database, 'Brussels', 'Munich', private_2)
+print(fleet_database, '\n')
+
+operate_flight(fleet_database, 'Prague', 'Munich', cargo_4)
+print(fleet_database, '\n')
+
+print(flights_log_database)
+
+
+# GENERATE PAIRS
+
+check = generate_pairs()
+print(next(check))
+print(next(check))
+print(next(check))
+print(next(check))
+print(next(check))
+print(next(check))
+print(next(check))
+# print(next(check))
+# print(next(check))
+# print(next(check))
+# print(next(check))
+
+print(fleet_database, '\n')
