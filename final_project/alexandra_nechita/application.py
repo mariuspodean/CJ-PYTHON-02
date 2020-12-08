65c7720263e148d9e487e78e0ca94aa5403755f6
Alexandra <alexandra.m.nechita@gmail.com>
2020-12-09 01:04:52 +0200

    requierements and tests files added, updated code with functionality

--- final_project/alexandra_nechita/application.py
+++ final_project/alexandra_nechita/application.py
@@ -3,67 +3,18 @@ import datetime
 
 eps = 0.0000001
 
-# logging.basicConfig(level=logging.DEBUG, filename=f'{datetime.datetime.now()}_Equilibrium_log.log')
-# logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG, filename=f'{datetime.datetime.now().strftime("%Y-%m-%d")}_Equilibrium_log.log')
+logger = logging.getLogger(__name__)
 
 class PersonalDevelopment:
     
 # Parent class. Mutable mapping type. It has 5 children classes: Physical, Spiritual, Emotional, Social, Intellectual.
    
     
-    def __init__(self, name=None, dict_of_activities=None):
+    def __init__(self, name = None, dict_of_activities = None):
         self.name = name
         self.dict_of_activities = dict_of_activities
         
-    def __eq__(self, other):
-        """
-        Operator == overloading. The method compares two instances of the PersonalDevelopment class
-        :param other:
-        :return: boolean flag weather self and other are the same or not
-        """
-        for activity, frequency in self.dict_of_activities.items():
-            if activity not in other.dict_of_activities:
-                return False
-            else:
-                if frequency != other.dict_of_activities[activity]:
-                    return False
-        return True
-    
-    def __ne__(self, other):
-        """
-        Operator != overloading. The method compares two instances of the PersonalDevelopment class
-        :param other:
-        :return: the negation of the __eq__ method returned boolean 
-        """
-        return not self.__eq__(other)
-    
-    def __lt__(self,other):
-        """
-        Operator < overloading. The method compares two instances of the PersonalDevelopment class, should not be comutative. The first
-        element is always representing the desired state and the second element is always the actual state.
-        :param other:
-        :return: the negation of __eq__ and __gt__ methods returned boolean
-        """
-        return not self.__eq__(other) and not self.__gt__(other)
-       
-    def __gt__(self, other):
-        """
-        Operator > overloading. The method compares two instances of the PersonalDevelopment class, should not be comutative. The first
-        element is always representing the desired state and the second element is always the actual state.
-        :param other:
-        :return: boolean flag weather self is greater than or not
-        """
-        sum_of_percentages = 0
-
-        for activity, frequency in self.dict_of_activities.items():
-            if activity not in other.dict_of_activities:
-                return True
-            else:
-                dif = frequency - other.dict_of_activities[activity]
-                percentage_of_frequencies = dif / max(frequency, eps) * 100
-                sum_of_percentages += percentage_of_frequencies
-        return sum_of_percentages > 0
-    
     def __getitem__(self, activity):
         return self.dict_of_activities[activity]
         
@@ -78,43 +29,93 @@ class PersonalDevelopment:
         
     def __len__(self):
         return len(self.dict_of_activities)
+        
+    def __eq__(self, other):
+# Operator == overloading. The method compares two instances of the PersonalDevelopment class
+# :param other:
+# :return: boolean flag weather self and other are the same or not
+# Two PersonalDevelopment objects are equal if they have identical list of activities and identical frequencies for each activity
+   
+        for activity, frequency in self.dict_of_activities.items():
+            if activity not in other.dict_of_activities:
+                return False
+            else:
+                if frequency != other.dict_of_activities[activity]:
+                    return False
+        return True
+    
+    def __ne__(self, other):
+# Operator != overloading. The method compares two instances of the PersonalDevelopment class
+# :param other:
+# :return: the negation of the __eq__ method returned boolean
+# Two PersonalDevelopment objects are not equal if they do not have identical list of activities and they do not have
+# identical frequencies for each activity
+        
+        return not self.__eq__(other)
+    
+    def __lt__(self, other):
+# Operator < overloading. The method compares two instances of the PersonalDevelopment class, should not be comutative. The first
+# element is always representing the desired state and the second element is always the actual state.
+# :param other:
+# :return: the negation of __eq__ and __gt__ methods returned boolean
+# An object is lesser than another if the set of activities of the first one is included in the set of activities of the second one,
+# but the sum of frequencies of the intersected elements of both sets is lesser in the first one than in the second one
+        self_set = set(self.dict_of_activities.keys())
+        other_set = set(other.dict_of_activities.keys())
+        if not self_set.issubset(other_set):
+            return False
+        else:
+            intersection_set = self_set & other_set
+            self_freq = 0
+            other_freq = 0
+            for element in intersection_set:
+                self_freq += self.dict_of_activities[element]
+                other_freq += other.dict_of_activities[element]
+            if self_freq > other_freq:
+                return False
+        return True
+                       
+    def __gt__(self, other):
+# Operator > overloading. The method compares two instances of the PersonalDevelopment class and it should not be comutative. The first
+# element is always representing the desired state and the second element is always the actual state.
+# :param other:
+# :return: boolean flag weather self is greater than or not
+# An object is greater than another if the set of activities of the first one includes or is the same as the set of activities
+# of the second one and the sum of frequencies of the intersected elements of both sets is greater in the first one
+# than in the second one
+        return not self.__eq__(other) and not self.__lt__(other)
     
     def save_state(self):
-        with i_am_a_context_manager(f'{self.name}.txt') as file:
+# Method to write in a file the activities implemented in a field
+        with i_am_a_context_manager(f'{datetime.datetime.now().strftime("%Y-%m-%d")}_{self.name}.txt') as file:
             file.write('Placeholder for list of activities')
 
 
 class MixinPrint:
-    """
-    Mixin class. Its only purpose is to make a nice print for Physical, Spiritual, Emotional, Social and Intellectual classes
-    """
-
+# Mixin class. Its only purpose is to make a nice print for Physical, Spiritual, Emotional, Social and Intellectual classes, using the
+# overwriting of __str__ method in child class
+   
     def __init__(self):
         pass
     
     def __str__(self, name='No_name', dict_of_activities={'no_activity': 0}, **kwargs):
         star_line = '*' * (len(name) + 4) + '\n'
-        string = f'{star_line} \n {name} \n {star_line} \n'
+        string = f'{star_line} \n{name} \n\n{star_line} \n'
         for index, (keys, values) in enumerate(dict_of_activities.items(), 1):
             string += f'{index}. {keys}: {values} \n'
         for indexx, (k,v) in enumerate(kwargs.items(), index+1):
-            string += f'{indexx}. {k}: {v}'
-        string += f'\n {star_line}'
+            string += f'{indexx}. {k}: {v}\n'
+        string += f'\n{star_line}\n'
         return (string)
 
 
 class Physical(PersonalDevelopment, MixinPrint):
-    """
-    Inherits from PersonalDevlopment class and MixinPrint
-    """
+# Inherits from PersonalDevlopment class and MixinPrint class
     
     def __init__(self, name, dict_of_activities, BMI):
         super().__init__(name, dict_of_activities)
         self.BMI = BMI
-            
-    def save_state(self):
-        super().save_state()
-    
+               
     def __getitem__(self, activity):
         return super().__getitem(self, activity)
         
@@ -137,10 +138,13 @@ class Physical(PersonalDevelopment, MixinPrint):
         return not self.__eq__(other)
            
     def __gt__(self, other):
-        return super().__gt__(other)
+        return not self.__eq__(other) and not self.__lt__(other)
     
     def __lt__(self,other):
-        return not self.__eq__(other) and not self.__gt__(other)
+        return super().__lt__(other) 
+    
+    def save_state(self):
+        super().save_state()
            
     def items(self):
         return self.dict_of_activities.items()
@@ -155,13 +159,11 @@ class Physical(PersonalDevelopment, MixinPrint):
         return super().__str__(self.name, self.dict_of_activities, BMI=self.BMI)
     
     def __repr__(self):
-        return (self.name, self.dict_of_activities, self.BMI)
+        return (f'The representation of the object Physical is: ({self.name}, {self.dict_of_activities}, {self.BMI}')
         
     
 class Spiritual(PersonalDevelopment, MixinPrint):
-    """
-    Inherits from PersonalDevlopment class and MixinPrint
-    """
+# Inherits from PersonalDevlopment class and MixinPrint
     
     def __init__(self, name, dict_of_activities, spiritual_views):
             super().__init__(name, dict_of_activities)
@@ -189,10 +191,10 @@ class Spiritual(PersonalDevelopment, MixinPrint):
         return not self.__eq__(other)
            
     def __gt__(self, other):
-        return super().__gt__(other)
+        return not self.__eq__(other) and not self.__lt__(other)
     
     def __lt__(self,other):
-        return not self.__eq__(other) and not self.__gt__(other)
+        return super().__lt__(other) 
     
     def save_state(self):
         super().save_state()
@@ -210,12 +212,11 @@ class Spiritual(PersonalDevelopment, MixinPrint):
         return super().__str__(self.name, self.dict_of_activities, spiritual_views=self.spiritual_views)
     
     def __repr__(self):
-        return (self.name, self.dict_of_activities, self.spiritual_views)
+        return (f'The representation of the object Spiritual is: ({self.name}, {self.dict_of_activities}, {self.spiritual_views}')
+
 
 class Emotional(PersonalDevelopment, MixinPrint):
-    """
-    Inherits from PersonalDevlopment class and MixinPrint
-    """
+# Inherits from PersonalDevlopment class and MixinPrint
     
     def __init__(self, name, dict_of_activities, EQ):
             super().__init__(name, dict_of_activities)
@@ -235,10 +236,7 @@ class Emotional(PersonalDevelopment, MixinPrint):
         
     def __len__(self):
         return super().__len__(self)
-    
-    def save_state():
-        super().save_state(self)
-    
+        
     def __eq__(self, other):
         return super().__eq__(other)
     
@@ -246,10 +244,13 @@ class Emotional(PersonalDevelopment, MixinPrint):
         return not self.__eq__(other)
            
     def __gt__(self, other):
-        return super().__gt__(other)
+        return not self.__eq__(other) and not self.__lt__(other)
     
-    def __lt__(self, other):
-        return not self.__eq__(other) and not self.__gt__(other)
+    def __lt__(self,other):
+        return super().__lt__(other) 
+    
+    def save_state(self):
+        super().save_state()
     
     def items(self):
         return self.dict_of_activities.items()
@@ -264,13 +265,11 @@ class Emotional(PersonalDevelopment, MixinPrint):
         return super().__str__(self.name, self.dict_of_activities, EQ=self.EQ)
     
     def __repr__(self):
-        return (self.name, self.dict_of_activities, self.EQ)
+        return (f'The representation of the object Emotional is: ({self.name}, {self.dict_of_activities}, {self.EQ}')
     
 
 class Social(PersonalDevelopment, MixinPrint):
-    """
-    Inherits from PersonalDevlopment class and MixinPrint
-    """
+# Inherits from PersonalDevlopment class and MixinPrint
     
     def __init__(self, name, dict_of_activities, dict_of_friends):
             super().__init__(name, dict_of_activities)
@@ -293,18 +292,18 @@ class Social(PersonalDevelopment, MixinPrint):
     
     def __eq__(self, other):
         return super().__eq__(other)
-    
-    def save_state(self):
-        super().save_state()
-    
+        
     def __ne__(self, other):
         return not self.__eq__(other)
            
     def __gt__(self, other):
-        return super().__gt__(other)
+        return not self.__eq__(other) and not self.__lt__(other)
     
     def __lt__(self,other):
-        return not self.__eq__(other) and not self.__gt__(other)
+        return super().__lt__(other) 
+    
+    def save_state(self):
+        super().save_state()
     
     def items(self):
         return self.dict_of_activities.items()
@@ -319,13 +318,11 @@ class Social(PersonalDevelopment, MixinPrint):
         return super().__str__(self.name, self.dict_of_activities, dict_of_friends=self.dict_of_friends)
     
     def __repr__(self):
-        return (self.name, self.dict_of_activities, self.dict_of_friends)
+        return (f'The representation of the object Social is: ({self.name}, {self.dict_of_activities}, {self.dict_of_friends}')
     
 
 class Intellectual(PersonalDevelopment, MixinPrint):
-    """
-    Inherits from PersonalDevlopment class and MixinPrint
-    """
+# Inherits from PersonalDevlopment class and MixinPrint
         
     def __init__(self, name, dict_of_activities, field_of_interest):
         super().__init__(name, dict_of_activities)
@@ -345,10 +342,7 @@ class Intellectual(PersonalDevelopment, MixinPrint):
         
     def __len__(self):
         return super().__len__(self)
-    
-    def save_state(self):
-        super().save_state()
-    
+       
     def __eq__(self, other):
         return super().__eq__(other)
     
@@ -356,10 +350,13 @@ class Intellectual(PersonalDevelopment, MixinPrint):
         return not self.__eq__(other)
            
     def __gt__(self, other):
-        return super().__gt__(other)
+        return not self.__eq__(other) and not self.__lt__(other)
     
     def __lt__(self,other):
-        return not self.__eq__(other) and not self.__gt__(other)
+        return super().__lt__(other) 
+    
+    def save_state(self):
+        super().save_state()
     
     def items(self):
         return self.dict_of_activities.items()
@@ -374,12 +371,11 @@ class Intellectual(PersonalDevelopment, MixinPrint):
         return super().__str__(self.name, self.dict_of_activities, field_of_interest=self.field_of_interest)
     
     def __repr__(self):
-        return (self.name, self.dict_of_activities, self.field_of_interest)
+        return (f'The representation of the object Intellectual is: ({self.name}, {self.dict_of_activities}, {self.field_of_interest}')
 
 class DomainsCollection():
-    """
-    Sequence like class(tuple). Holds PersonalDevelopment like objects
-    """
+# Sequence-like class(tuple). Holds PersonalDevelopment objects
+
     def __init__(self, name, domains_collection):
         self.name = name
         self.domains_collection = domains_collection
@@ -397,29 +393,32 @@ class DomainsCollection():
         return (f'{self.name} contains the following domains: {domains_names_list}')
     
     def __repr__(self):
-        return (self.name, self.domains_collection)
+        return (f'The representation of the object {self} is {self.name} = {self.domains_collection}')
+    
 
 def i_am_a_decorator(function):
+# This function will be used to decorate the check_status function 
+
     def inner_function(*args):
         printing_list = function(*args)
         for (domain, status) in printing_list:
             if status == 'equal':
-                print(f'\u001b[32m\u2b9e\u001b[0m Congrats! \n You reached your goal on {domain}! \n Keep up the good work! ')
+                print(f'\n \u001b[32m\u2b9e\u001b[0m Congrats! \n You reached your goal on {domain}! \n Keep up the good work!')
             elif status == 'greater':
-                print(f'\u001b[36m\u2b9d\u001b[0m Wow! \n You exceeded the expectations on {domain}! \n Keep it all in equilibrium! ')
+                print(f'\n \u001b[36m\u2b9d\u001b[0m Wow! \n You exceeded the expectations on {domain}! \n Keep it all in equilibrium!')
             else:
-                print(f'\u001b[31m\u2b9f\u001b[0m You did not reach your goal this week on {domain}! \n Level up next week!')
+                print(f'\n \u001b[31m\u2b9f\u001b[0m You did not reach your goal this week on {domain}! \n Level up next week!' + '\n')
         return function
         
     return inner_function
+
     
 @i_am_a_decorator
 def check_status(desired_state_collection, actual_state_collection):
-    """
-    Function compares two DomainsCollection-like objects, member by member
-    :param desired_state_collection, actual_state_collection
-    :return: a list with five tuples: desired state - status of the actual state compared with the desired state
-    """
+# Function compares two DomainsCollection objects, member by member
+# :param desired_state_collection, actual_state_collection
+# :return: a list with five tuples: desired state - status of the actual state compared with the desired state
+
     printing_list = []
     for index in range(5):
         if desired_state_collection[index] == actual_state_collection[index]:
@@ -429,11 +428,12 @@ def check_status(desired_state_collection, actual_state_collection):
         else:
             printing_list.append((desired_state_collection[index].name, 'greater'))
     return printing_list
+
         
 class i_am_a_context_manager():
-    """
-    Implementation of a context manager that opens a file in __enter__method and closes it in __exit__ method. No errors expected.
-    """
+# Implementation of a context manager that opens a file in __enter__ method (in order to write something in it) and
+# closes it in __exit__ method. No errors expected. The context manager is used in the method save_state in every of the five children
+# classes of PersonalDevelopment
     
     def __init__(self, file_name):
         self.file_name = file_name
@@ -445,7 +445,9 @@ class i_am_a_context_manager():
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.file.close()
         
+        
 def i_am_a_generator(dict_of_friends):
+# Implementation of a generator factory that will yield a key from a dictionary - called in playground.py
     for friend in dict_of_friends.keys():
         yield friend
     
--- final_project/alexandra_nechita/playground.py
+++ final_project/alexandra_nechita/playground.py
@@ -3,39 +3,6 @@ import datetime
 from time import sleep
 from inspect import getgeneratorstate
 
-
-Alexandra_desired_physical = {
-    'sleep': 56,
-    'nutrition': 10500,
-    'physical_exercise': 7
-}
-
-Alexandra_desired_spiritual = {
-    'charity': 1,
-    'meditation': 7,
-    'contemplation': 1
-}
-
-Alexandra_desired_emotional = {
-    'identifying_emotions_in_self': 1,
-    'identifying_emotions_in_others': 3,
-    'managing_self_emotions': 3,
-    'managing_relationships': 7
-}
-
-Alexandra_desired_social = {
-    'monthly_message': 4,
-    'monthly_meeting': 1,
-    'randomly_calls': 2
-}
-
-Alexandra_desired_intellectual = {
-    'daily_reading': 50,
-    'memory_exercises': 10,
-    'foreign_language': 105,
-    'online_classes': 1
-}
-
 Alexandra_dict_of_friends = {
     'Dan': ('0722222222', datetime.datetime(1988, 12, 5).strftime("%x")),
     'Larisa': ('0745111111', datetime.datetime(1991, 7, 5).strftime("%x")),
@@ -43,141 +10,50 @@ Alexandra_dict_of_friends = {
     'Dan Adrian': ('0721555555', datetime.datetime(1966,11, 9).strftime("%x"))
 }
 
-Alexandra_actual_physical = {}
-Alexandra_actual_spiritual = {}
-Alexandra_actual_emotional = {}
-Alexandra_actual_social = {
-    'monthly_message': 6,
-    'monthly_meeting': 1,
-    'randomly_calls': 2
-}
-Alexandra_actual_intellectual = {
-    'daily_reading': 50,
-    'memory_exercises': 10,
-    'foreign_language': 105,
-    'online_classes': 1
-}
+Ale_DP = Physical('Desired Physical', {'sleep': 56, 'nutrition': 10500, 'physical_exercise': 7}, 23.0)
+Ale_DSp = Spiritual('Desired Spiritual', {'charity': 1, 'meditation': 7, 'contemplation': 1}, 'buddhist')
+Ale_DE = Emotional('Desired Emotional', {'identifying_emotions_in_self': 1, 'identifying_emotions_in_others': 3, 'managing_self_emotions': 3, 'managing_relationships': 7}, 120)
+Ale_DSo = Social('Desired Social', {'monthly_message': 4, 'monthly_meeting': 1, 'randomly_calls': 2}, Alexandra_dict_of_friends)
+Ale_DI = Intellectual('Desired Intellectual', {'daily_reading': 50, 'memory_exercises': 10, 'foreign_language': 105, 'online_classes': 1}, 'computer science')
 
-Alexandra_Actual_Physical = Physical('Alexandra\'s Actual Physical', Alexandra_actual_physical, 23.0)
-Alexandra_Actual_Spiritual = Spiritual('Alexandra\'s Actual Spiritual', Alexandra_actual_spiritual, 'buddhist')
-Alexandra_Actual_Emotional = Emotional('Alexandra\'s Actual Emotional', Alexandra_actual_emotional, 120)
-Alexandra_Actual_Social = Social('Alexandra\'s Actual Social', Alexandra_actual_social, Alexandra_dict_of_friends)
-Alexandra_Actual_Intellectual = Intellectual('Alexandra\'s Actual Intellectual', Alexandra_actual_intellectual, 'computer science')
+Ale_AP = Physical('Actual Physical', {'sleep': 50, 'nutrition':10500, 'physical_exercise': 7}, 23.0)
+Ale_ASp = Spiritual('Actual Spiritual', {'charity': 1, 'meditation': 7, 'contemplation': 1}, 'buddhist')
+Ale_AE = Emotional('Actual Emotional',{'identifying_emotions_in_others': 3, 'managing_self_emotions': 3, 'managing_relationships': 7} , 120)
+Ale_ASo = Social('Actual Social',{'monthly_message': 6, 'monthly_meeting': 3, 'randomly_calls': 2} , Alexandra_dict_of_friends)
+Ale_AI = Intellectual('Actual Intellectual', {'daily_reading': 15, 'memory_exercises': 10, 'foreign_language': 105, 'online_classes': 1, 'group study': 1}, 'computer science')
 
-Alexandra_Desired_Physical = Physical('Alexandra\'s Desired Physical', Alexandra_desired_physical, 23.0)
-Alexandra_Desired_Spiritual = Spiritual('Alexandra\'s Desired Spiritual', Alexandra_desired_spiritual, 'buddhist')
-Alexandra_Desired_Emotional = Emotional('Alexandra\'s Desired Emotional', Alexandra_desired_emotional, 120)
-Alexandra_Desired_Social = Social('Alexandra\'s Desired Social', Alexandra_desired_social, Alexandra_dict_of_friends)
-Alexandra_Desired_Intellectual = Intellectual('Alexandra\'s Desired Intellectual', Alexandra_desired_intellectual, 'computer science')
 
-print(Alexandra_Desired_Physical)
-print(Alexandra_Desired_Spiritual)
-print(Alexandra_Desired_Emotional)
-print(Alexandra_Desired_Social)
-print(Alexandra_Desired_Intellectual)
-print(Alexandra_Desired_Intellectual.__repr__())
+print(f'{Ale_DP} \n{Ale_DSp} \n{Ale_DE} \n{Ale_DSo} \n{Ale_DI}')
 
-intellectual_activities = list(Alexandra_Desired_Intellectual.keys())
-print('The activities list for intellectual domain is', intellectual_activities, '\n')
+print(Ale_DI.__repr__(), '\n')
 
-intellectual_activities_and_details = list(Alexandra_Desired_Intellectual.items())
-print('The activities list for intellectual domain, with the corresponding details, is', intellectual_activities_and_details, '\n')
 
-Alexandra_desired_collection = (Alexandra_Desired_Physical, Alexandra_Desired_Spiritual, Alexandra_Desired_Emotional, Alexandra_Desired_Social, Alexandra_Desired_Intellectual)
-Alexandra_actual_collection = (Alexandra_Actual_Physical, Alexandra_Actual_Spiritual, Alexandra_Actual_Emotional, Alexandra_Actual_Social, Alexandra_Actual_Intellectual)
+Ale_DCollection = DomainsCollection('Alexandra\'s Desired Collection', (Ale_DP, Ale_DSp, Ale_DE, Ale_DSo, Ale_DI))
+Ale_ACollection = DomainsCollection('Alexandra\'s Actual Collection', (Ale_AP, Ale_ASp, Ale_AE, Ale_ASo, Ale_AI))
 
-Alexandra_Desired_Collection = DomainsCollection('Alexandra\'s Desired Collection', Alexandra_desired_collection)
-Alexandra_Actual_Collection = DomainsCollection('Alexandra\'s Actual Collection', Alexandra_actual_collection)
-print(Alexandra_Desired_Collection)
+print(Ale_DCollection)
 
-Dan_physical = {
-    'sleep': 56,
-    'nutrition': 10500,
-    'physical_exercise': 7
-}
 
-Dana_physical = {
-    'sleep': 50,
-    'nutrition': 10500,
-    'physical_exercise': 7
-}
+print(f'Desired state on physical is greater than actual state: {Ale_DP > Ale_AP}')
+print(f'Desired state on spiritual is equal with actual state: {Ale_DSp == Ale_ASp}')
+print(f'Desired state on emotional is greater than actual state: {Ale_DE > Ale_AE}')
+print(f'Desired state on social is lesser than actual state: {Ale_DSo < Ale_ASo}')
+print(f'Desired state on intellectual is lesser than actual state: {Ale_DI < Ale_AI}')
 
-Darius_physical = {
-    'sleep': 60,
-    'nutrition': 10500,
-    'physical_exercise': 7
-}
 
-Mara_physical = {
-    'sleep': 57,
-    'nutrition': 10500,
-    'physical_exercise': 7,
-    'medical check': 1
-}
+check_status(Ale_DCollection, Ale_ACollection)
 
-Mihai_physical = {
-    'sleep': 56,
-    'nutrition': 10500,
-    'physical_exercise': 7,
-    'medical check': 1
-}
-
-Mioara_physical = {
-    'sleep': 50,
-    'nutrition': 10500,
-    'physical_exercise': 7,
-    'medical check': 1
-}
-
-Bogdan_physical = {
-    'sleep': 80,
-    'nutrition': 10500
-    
-}
-
-Ioana_physical = {
-    'sleep': 64,
-    'nutrition': 10500
-    
-}
-
-Andreea_physical = {
-    'sleep': 63,
-    'nutrition': 10500
-    
-}
-
-Dan_Physical = Physical('Dan\'s Physical', Dan_physical, 23)
-Dana_Physical = Physical('Dana\'s Physical', Dana_physical, 23)
-Darius_Physical = Physical('Darius\'s Physical', Darius_physical, 23)
-Mara_Physical = Physical('Mara\'s Physical', Mara_physical, 23)
-Mihai_Physical = Physical('Mihai\'s Physical', Mihai_physical, 23)
-Mioara_Physical = Physical('Mioara\'s Physical', Mioara_physical, 23)
-Bogdan_Physical = Physical('Bogdan\'s Physical', Bogdan_physical, 23)
-Ioana_Physical = Physical('Ioana\'s Physical', Ioana_physical, 23)
-Andreea_Physical = Physical('Andreea\'s Physical', Andreea_physical, 23)
-
-print(Alexandra_Desired_Physical==Dan_Physical)
-print(Alexandra_Desired_Physical>Dana_Physical)
-print(Alexandra_Desired_Physical<Darius_Physical)
-print(Alexandra_Desired_Physical>Mara_Physical)
-print(Alexandra_Desired_Physical<Mioara_Physical)
-print(Alexandra_Desired_Physical==Mihai_Physical)
-print(Alexandra_Desired_Physical>Bogdan_Physical)
-print(Alexandra_Desired_Physical<Ioana_Physical)
-print(Alexandra_Desired_Physical==Andreea_Physical)
-
-check_status(Alexandra_Desired_Collection, Alexandra_Actual_Collection)
-
-Alexandra_Desired_Physical.save_state()
+Ale_DP.save_state()
 
 gen = i_am_a_generator(Alexandra_dict_of_friends)
 
+# The code block below should be included in a coroutine in order not to block the execution of the app
 while True:
     try:
         print(getgeneratorstate(gen))
-        print(f'Please suggest this app to your friend {next(gen)}')
+        print(f'Please recommend \u001b[35mEquilibrium\u001b[0m app to your friend, {next(gen)}')
         sleep(2)
     except StopIteration:
-        print('Thank you for suggesting us to all you friends')
+        print('Thank you for recommending us to all your friends!\u2665')
+        print(getgeneratorstate(gen))
         break
