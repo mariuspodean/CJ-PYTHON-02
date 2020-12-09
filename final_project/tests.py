8ab6f38c5d2e0572e035122abee8a6d8d5c59006
Oana Agafitei <oana_agafitei@yahoo.com>
2020-12-09 15:10:48 +0200

    Remove unneeded files and add additional unit test

--- final_project/tests.py
+++ final_project/tests.py
@@ -29,5 +29,18 @@ class BouquetTest(unittest.TestCase):
         assert result.bouquet_flowers["flower 1"] == 2
         assert result.bouquet_flowers["flower 2"] == 5
 
+    def test_adding_two_partially_overlapping_bouquets(self):
+        b1 = Bouquet("Bouquet 1",{"flower 1":1,"flower 2":2})
+        b2 = Bouquet("Bouquet 2",{"flower 1":1,"flower 3":3})
+        result = b1 + b2
+        
+        assert result.bouquet_name == "Bouquet 1 and Bouquet 2"
+        assert result.bouquet_flowers.__contains__("flower 1")
+        assert result.bouquet_flowers.__contains__("flower 2")
+        assert result.bouquet_flowers.__contains__("flower 3")
+        assert result.bouquet_flowers["flower 1"] == 2
+        assert result.bouquet_flowers["flower 2"] == 2
+        assert result.bouquet_flowers["flower 3"] == 3
+
 if __name__ == '__main__':
     unittest.main()
