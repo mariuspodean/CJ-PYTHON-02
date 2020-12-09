1acf4feb071a48925a7c7645c11bbf505314ec85
Oana Agafitei <oana_agafitei@yahoo.com>
2020-12-09 13:48:36 +0200

    First Part of Implementation

--- final_project/application.py
+++ final_project/application.py
@@ -3,19 +3,58 @@ import time
 from random import randint
 from contextlib import contextmanager
 from random import randrange
+from datetime import datetime
 
+now = datetime.now()
 
-procurement_list_archive=[]
+# logging settings
+logging.basicConfig(level=logging.DEBUG,filemode="a",filename="history.txt")
 
+# Decorator
+def log_operation(function):
+
+    def wrapper(*args, **kwargs):
+        time = now.strftime("[%Y-%m-%D %H:%M:%S]")
+        logging.debug(time + "Operation performed:"+ repr(function))
+        for arg in args:
+            logging.debug(time + "Parameter : " + repr(arg))
+        return function(*args,**kwargs)
+
+    return wrapper
+
+# Mutable Mapping
 class Bouquet:
 
     def __init__(self, bouquet_name=None, bouquet_flowers=None):
         self.bouquet_name = bouquet_name
         self.bouquet_flowers = bouquet_flowers
-
+    
+    # String and representation implemented 
     def __repr__(self):
         print_string = f'{self.bouquet_name}: {self.bouquet_flowers}'
         return print_string
+
+    def __str__(self):
+        print_string = ''
+        bouquet_title = f'{self.bouquet_name}\n'
+        print_string = ''.join((print_string, bouquet_title))
+        return print_string
+
+    # Operator overloading
+    # Decorators usage
+    @log_operation
+    def __add__(self, other):
+        bouquet_name = self.bouquet_name + " and " + other.bouquet_name
+        bouquet_flowers = {}
+        for b1 in self.bouquet_flowers:
+            bouquet_flowers[b1]=self.bouquet_flowers[b1]
+        for b2 in other.bouquet_flowers:
+            if bouquet_flowers.get(b2) != None :
+                bouquet_flowers[b2] += other.bouquet_flowers[b2]
+            else :
+                bouquet_flowers[b2] = other.bouquet_flowers[b2]
+
+        return Bouquet(bouquet_name,bouquet_flowers)
 
     def __len__(self):
         return len(self.bouquet_flowers)
@@ -39,10 +78,19 @@ class Bouquet:
         return self.bouquet_flowers.items()
 
 
+# Sequence
 class BouquetBox:
 
-    def __init__(self):
-        self._bouquetBox_list = []
+    def __init__(self, stock):
+        self._bouquetBox_list = stock
+
+    # String and representation implemented
+    def __repr__(self):
+        representation ='['
+        for bouquet in self._bouquetBox_list : 
+            representation += '{' + f'{bouquet.bouquet_name}:{bouquet.bouquet_flowers}' +'}'
+        representation += ']'
+        return representation
 
     def __str__(self):
         print_string = ''
@@ -89,100 +137,6 @@ class BouquetBox:
 
         return self._bouquetBox_list[index]
 
-class Stock:
-    
-    def __init__(self,name=None, inside_stock=None):
-        self.inside_stock = inside_stock
-        self.name = name
-       
-# When printing the stock object, it will print similar to a bouquet, its content.
-    def __str__(self):
-        print_content = ''
-        for flower in self.inside_stock:
-            print_content = ' '.join((print_content, flower, str(self.inside_stock[flower]))) 
-            print_content = f'{print_content}\n'
-        return print_content
-
-
-
-# def __repr__(self):
-#         print_content = ''
-#         for flower in self.inside_stock:
-#             print_content = ' '.join((print_content, flower, str(self.inside_stock[flower]))) 
-#             print_content = f'{print_content}\n'
-#         return print_content
-
-
-
-
-#  mutable mapping
-    def __contains__(self, flower):
-        return flower in self.inside_stock
-
-# We can also ask if a certain product is in stock
-    def check_flower(self, flower):
-        if flower in self.inside_stock:
-            print ('Flower in stock')
-    
-    def __iter__(self):
-        return iter(self.inside_stock)
-
-    def __len__(self):
-        return len(self.inside_stock)
-
-    def __getitem__(self, flower):
-        return self.inside_stock[flower]
-
-    def __setitem__(self, flower, quantity):
-        self.inside_stock[flower] = quantity
-
-    def __delitem__(self, flower):
-        del self.inside_stock[flower]
-
-# We can add new items in the stock.
-    def add_flower_in_stock(self, flower, quantity):
-        if flower in self.inside_stock:
-            self.inside_stock[flower] += quantity
-        else:
-            self.inside_stock[flower] = quantity
-
-# We can [...] remove existing ones. Tot aici actualizam cantitatile in minus. Anuntam cand ramanem fara ceva in stock
-    def remove_flower_from_stock(self, flower, quantity):
-        if flower in self.inside_stock:
-            self.inside_stock[flower] = self.inside_stock[flower] - quantity
-            if self.inside_stock[flower] <= 0:
-                print (f'No more {flower} in stock.')
-                del self.inside_stock[flower]
-        else:
-            print ('No such flower in stock')
-    
-    def check_bouquet(self,bouquet)->[]:
-        list_we_have = []
-        list_to_shop = []
-        for flower in bouquet.keys():
-            if flower in self:
-                if self[flower] >= bouquet[flower]:
-                    list_we_have.append(flower)
-                else:
-                    quantity_to_buy = bouquet[flower] - self[flower]
-                    list_to_shop.append((flower, quantity_to_buy))
-            else:
-                list_to_shop.append((flower,bouquet[flower]))
-        print (f'We already have: {list_we_have}')
-        print (f'We need to buy: {list_to_shop}')
-        return list_to_shop
-
-    def update(self,flower):
-        if(self.inside_stock is None):
-            self.inside_stock = {}
-        for flower in flower.items():
-            (name,quantity)= flower
-            self.inside_stock[name]= quantity
-    
-    def update_quantity(self, flower,quantity):
-        pass
-                
-
 class PrettyPrinter():
 
     def __init__(self,name=None,flowers=None):
@@ -201,6 +155,7 @@ _.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._
         spacer = '''
  ) )                                                       ( ('''
         to_print=header
+        print('++++++++++++++++++')
         if(self.name):
             to_print+= self.__replace_line__(self.name) + spacer
         else:
@@ -218,151 +173,54 @@ _.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._
         to_replace = len(replace_with)*' '
         return filler.replace(to_replace,replace_with,1)
 
+# Inheritance + Mixin
 class PrettyBouquet(PrettyPrinter,Bouquet):
     pass
 
+# Context Manager + Generator
+class BouquetManager(): 
+    def __init__(self): 
+        print('Welcome to the Bouquet store')
+        
+    def __enter__(self): 
+        print('I am the Bouquet Manager.') 
+        return self
+      
+    def __exit__(self, exc_type, exc_value, exc_traceback): 
+        print('Thank you for comming!')
+        print('Goodbye!')
 
-class PrettyStock(PrettyPrinter,Stock):
-    pass
-
-def check_the_stock(stock, bouquets):
-    half_or_more_flowers_bouquets = []
-    less_than_half_flowers_bouquets = []
-    for bouquet in bouquets:
-        available_flowers = 0
-        for flower in bouquets.keys():
-            if flower in stock:
-                available_flowers += 1
-        if available_flowers >= (len(bouquet.keys())/2):
-            half_or_more_flowers_bouquets.append(bouquet.bouquet_name)
-        else:
-            less_than_half_flowers_bouquets.append(bouquet.bouquet_name)
-    print(f'We have half or more flowers for: {half_or_more_flowers_bouquets}')
-    print(f'We have less than half flowers for: {less_than_half_flowers_bouquets}')
-    return half_or_more_flowers_bouquets          
-
-# check_the_stock(our_stock, our_bouquets_box)
-
-def pretty_print_bouquet(function):
-
-    def wrapper(stock,bouquet):
-        procurement_list=function(stock,bouquet)
-        missing_items = PrettyBouquet("Flowers needed",procurement_list)
-        missing_items.pretty_print()
-        return procurement_list
-
-    return wrapper
-
-def archive_procurement_list(fnc2):
-    def inner_func_2(stock, bouquet):
-        procurement_list = fnc2(stock, bouquet)
-        time_now = time.localtime()
-        date_today = time.strftime('%d %m %Y', time_now)
-
-        if type(procurement_list) == dict:
-            procuremet_list_archive.append((date_today, bouquet.bouquet_name, procurement_list))
-
-        return procurement_list
-
-    return inner_func_2
-
-@archive_procurement_list
-@pretty_print_bouquet
-def prepare_procurement_list(stock, bouquet):
-    procurement_list = {}
-    for flower in bouquet.keys():
-        if flower in stock:
-            if stock[flower] <= bouquet[flower]:
-                quantity_to_buy = bouquet[flower] - stock[flower]
-                procurement_list.update({flower: quantity_to_buy})
-    print (f'Procurement list: {procurement_list}')
-    return procurement_list
-
-# #OPERATOR OVERLOADING
-# class LilyBouquets:
-
-#     def __init__(self, bouquet):
-#         self.bouquet = bouquet
-
-#     def __str__(self):
-#         return self.bouquet
-
-#     def __add__(self, other):
-#         return SpecialBouquet(f'{self.bouquets + other.bouquet} concatenated from {self.bouquet} and {other.bouquet}')
-
-
-# class RosesBouquet:
-
-#     def __init__(self, bouquet):
-#         self.bouquet = bouquet
-
-
-
-
-# #context manager
-
-# #generator
-
-
-# class ComputerConfig(DisplayMixin):
-
-#     def __init__(self, name='noName', *args):
-#         self.name = name
-#         self.items = list(args)
-#         should_have = ['MTB', 'VID', 'PRO', 'RAM', 'SSD']
-#         for item in list(args):
-#             if item.part_type in should_have:
-#                 should_have.remove(item.part_type)
-#             else:
-#                 raise ValueError('you should have one of each component')
-#         if should_have:
-#             raise ValueError('you are missing : ' + str(should_have))
-#         if len(args) < 5:
-#             raise ValueError('the computer is missing components')
-#         self.price = 0
-#         for _ in list(args):
-#             self.price += _.price
-
-
-# config_list = []
-
-
-# def log_configurator(fnc):
-#     def inner_func(name, config_parts, values):
-#         configure_computer = fnc(name, config_parts, values)
-#         config_list.append(configure_computer.name)
-#         return configure_computer
-
-#     return inner_func
-
-# # logging
-# logger = logging.getLogger(__name__)
-# logging.basicConfig(level=logging.DEBUG)
-
-
-# # decorator
-# @log_configurator
-# def configure_computer(name, config_parts, values):
-#     start = time.time()
-#     proc = config_parts.pick_pro(values[0])
-#     mtbd = config_parts.pick_mtb(values[1])
-#     vide = config_parts.pick_vid(values[2])
-#     rams = config_parts.pick_ram(values[3])
-#     ssds = config_parts.pick_ssd(values[4])
-#     end = time.time()
-#     logger.info(f'configuration took {end - start} seconds')
-#     return ComputerConfig(name, proc, mtbd, vide, rams, ssds)
-
-
-# # context manager, decorator and generator
-# @contextmanager
-# def open_file(name):
-#     f = open(name, 'w')
-#     try:
-#         yield f
-#     finally:
-#         f.close()
-
-# def save_to_txt(txt='no text entered'):
-#     with open_file('computer_history.txt') as f:
-#         f.write(txt)
+def enter_store(stock):
+    with BouquetManager() as manager:
+        val = 'h'
+        items_bought=[]
+        while val != 'q': 
+            val = input("""How can I help you:
+            l - list stock
+            b - buy bouquet
+            m - merge all purchases
+            p - print purchases
+            q - good bye
+            """) 
+            if val == 'l':
+                print(str(stock))
+            elif val == 'b':
+                item = input(f'Buying item [0 to {len(stock)-1}]:')
+                items_bought.append(stock[int(item)]) 
+                yield stock[int(item)]
+            elif val == 'p':
+                print("Printing purchased items:")
+                for item in items_bought :
+                    print("----------")
+                    pretty_bouquet=PrettyBouquet(item.bouquet_name,item.bouquet_flowers)
+                    pretty_bouquet.pretty_print()
+            elif val == 'm':
+                print('Merging all purchased items:')
+                if len(items_bought) >0 :
+                    sum = items_bought[0]
+                    for item in items_bought[1:]:
+                        # operator overloading of +
+                        sum = sum +  item
+                items_bought = [sum]
+                print(items_bought)
+        return items_bought        
--- final_project/playground.py
+++ final_project/playground.py
@@ -1,18 +1,12 @@
-from application import Bouquet,PrettyBouquet, BouquetBox, Stock,PrettyStock, check_the_stock, \
-    prepare_procurement_list, procurement_list_archive
-
-print('--------------------------')
-print('Customer facing showcase')
-print('--------------------------')
-print('')
+from application import Bouquet, PrettyBouquet, BouquetBox, enter_store
 
 # create the flowers dict for multiple bouquets, containing flowers and quantities
 
 Christmas_Spirit_Bouquet_flowers = {
-'White Roses':3
-'White Bloom Chrysanthemums':5
-'White Tipped Pine Cones':3
-'Laurel':5
+    'White Roses':3,
+    'White Bloom Chrysanthemums':5,
+    'White Tipped Pine Cones':3,
+    'Laurel':5,
 }
 
 Christmas_Spirit_Bouquet = Bouquet(
@@ -21,27 +15,27 @@ Christmas_Spirit_Bouquet = Bouquet(
 )
 
 
-Smiles&Sunshine_Bouquet_flowers = {
-    'Peruvian Lily: 7,
-    'rose':9 
+Smiles_Sunshine_Bouquet_flowers = {
+    'Peruvian Lily': 7 ,
+    'Rose':9 
     }
 
-Smiles&Sunshine_Bouquet = Bouquet(
+Smiles_Sunshine_Bouquet = Bouquet(
     "Smiles & Sunshine Bouquet",
-    Smiles&Sunshine_Bouquet_flowers
+    Smiles_Sunshine_Bouquet_flowers
 )
 
 
 Christmas_Celebration_Bouquet_flowers = {
-    'Red Amaryllis':1
-    'Red Hypericum':1
-    'Purple Bloom Chrysanthemums':2
-    'Red Gerbera':2
-    'Purple Germini':3
-    'Red Spray Roses':2
-    'Burgundy Carnations':3
-    'Rai Cones':2
-    'Rhodo':2
+    'Red Amaryllis':1,
+    'Red Hypericum':1,
+    'Purple Bloom Chrysanthemums':2,
+    'Red Gerbera':2,
+    'Purple Germini':3,
+    'Red Spray Roses':2,
+    'Burgundy Carnations':3,
+    'Rai Cones':2,
+    'Rhodo':2,
     'Conifer':3
 }
 
@@ -51,37 +45,42 @@ Christmas_Celebration_Bouquet = Bouquet(
 )
 
 White_Medley_Bouquet_flowers= {
-    'White Spray Stocks':3
-    'White Rose':5
-    'White Double Lisianthus':5
-    'White Trachelium':5
+    'White Spray Stocks':3,
+    'White Rose':5,
+    'White Double Lisianthus':5,
+    'White Trachelium':5,
     'Eucalyptus':3
 }
 
-White_Medley_Bouquet_flowers = Bouquet(
+White_Medley_Bouquet = Bouquet(
     "White Medley Bouquet",
     White_Medley_Bouquet_flowers
 )
 
 Rainbow_Alstroemeria_Bouquet_flowers = {
-    'White Alstroemeria':3
-    'Pink Alstroemeria':3
-    'Yellow Alstroemeria':3
-    'Red Alstroemeria':3
-    'Orange Alstroemeria':2
+    'White Alstroemeria':3,
+    'Pink Alstroemeria':3,
+    'Yellow Alstroemeria':3,
+    'Red Alstroemeria':3,
+    'Orange Alstroemeria':2,
     'Cerise Alstroemeria':2
 }
 
-
-Rainbow_Alstroemeria_Bouquet_flowers = Bouquet(
+Rainbow_Alstroemeria_Bouquet = Bouquet(
     "Rainbow Alstroemeria_Bouquet",
     Rainbow_Alstroemeria_Bouquet_flowers
 )
-
-
-# create instances for each bouquet by its title and dict of ingredients
-print('\n\033[34m BOUQUETS \033[0m\n')
-
 Christmas_Spirit_Bouquet = PrettyBouquet("Christmas Spirit Bouquet", Christmas_Spirit_Bouquet_flowers)
-print(Christmas_Spirit_Bouquet)
-# print(Christmas_Spirit_Bouquet.__repr__())
+
+stock=[
+    Christmas_Spirit_Bouquet,
+    Smiles_Sunshine_Bouquet,
+    Christmas_Celebration_Bouquet,
+    White_Medley_Bouquet,
+    Rainbow_Alstroemeria_Bouquet
+]
+
+bouquet_box = BouquetBox(stock)
+
+for item in enter_store(bouquet_box):
+    print(str(item))
